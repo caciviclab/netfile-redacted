8bde9d0a854936951ea29d640273f2a5497eaa9d
ChenglimEar <ChenglimEar@users.noreply.github.com>
2023-09-13 19:34:19 -0700

    Update pull_and_redact_files.py

--- pull_and_redact_files.py
+++ pull_and_redact_files.py
@@ -1,1 +1,9 @@
+import os
+import json
+
 print('Replace this with python script to pull and redact files')
+
+os.makedirs('netfile_redacted')
+data = {'msg':'hello'}
+with open('netfile_redacted/data.json','w') as f:
+  json.dump(data,f)
