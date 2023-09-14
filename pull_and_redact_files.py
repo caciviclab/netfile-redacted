2955f6b7130269e85649f4a233713362e3e81a21
Chenglim Ear <chenglim_ear@yahoo.com>
2023-09-13 21:52:01 -0700

    switch code based on repo owner

--- pull_and_redact_files.py
+++ pull_and_redact_files.py
@@ -6,11 +6,11 @@ print('Replace this with python script to pull and redact files')
 
 os.makedirs('netfile_redacted', exist_ok=True)
 
-REPO_NAME = os.getenv('REPO_NAME')
+REPO_OWNER = os.getenv('REPO_OWNER')
 
-print(REPO_NAME)
+print(REPO_OWNER)
 
-if REPO_NAME == 'netfile_redacted':
+if REPO_OWNER == 'ChenglimEar':
     data = {'msg':'hello'}
     with open('netfile_redacted/data.json','w') as f:
         json.dump(data,f)
