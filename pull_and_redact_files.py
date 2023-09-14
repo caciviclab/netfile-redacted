c3de7bb4239f8f40620082ba1ba736f858104577
Chenglim Ear <chenglim_ear@yahoo.com>
2023-09-13 21:44:02 -0700

    add netfile_client

--- pull_and_redact_files.py
+++ pull_and_redact_files.py
@@ -1,9 +1,20 @@
 import os
 import json
+from netfile_client.NetFileClient import NetFileClient
 
 print('Replace this with python script to pull and redact files')
 
 os.makedirs('netfile_redacted')
-data = {'msg':'hello'}
-with open('netfile_redacted/data.json','w') as f:
-  json.dump(data,f)
+
+REPO_NAME = os.getenv('REPO_NAME')
+
+print(REPO_NAME)
+
+if REPO_NAME == 'netfile_redacted':
+    data = {'msg':'hello'}
+    with open('netfile_redacted/data.json','w') as f:
+        json.dump(data,f)
+else:
+    nf = NetFileClient()
+    nf.fetch('filings')
+
