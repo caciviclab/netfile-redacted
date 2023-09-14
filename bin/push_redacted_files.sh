6672be8cb478a457e31c9d0f1a477493db53542f
ChenglimEar <ChenglimEar@users.noreply.github.com>
2023-09-13 19:35:51 -0700

    Update push_redacted_files.sh

--- bin/push_redacted_files.sh
+++ bin/push_redacted_files.sh
@@ -1,1 +1,4 @@
 # Add bash script to push redacted files into this repository, which will likely live under a netfile_redacted directory
+
+echo "Pushing netfile_redacted to repo"
+ls netfile_redacted
