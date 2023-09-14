ceaaa568c0d143286ed700b133d002fec829f4e5
ChenglimEar <ChenglimEar@users.noreply.github.com>
2023-09-13 20:00:20 -0700

    Update push_redacted_files.sh

--- bin/push_redacted_files.sh
+++ bin/push_redacted_files.sh
@@ -1,15 +1,15 @@
 # Add bash script to push redacted files into this repository, which will likely live under a netfile_redacted directory
 
 echo "Pushing netfile_redacted to repo"
-ls netfile_redacted
+ls
 
 echo "Adding to repo using git"
 git status
+git branch
 git add netfile_redacted
 git config --global user.name 'OpenDisclosure Deploybot'
 git config --global user.email 'open-disclosure@gmail.com'
-git remote set-url origin git@github.com:ChenglimEar/test-dev-container.git
-git branch
 git status
-git commit -a -m "netfile update"
+echo "Committing"
+git commit -m "netfile update"
 git push
