a2d4eb9823cdf16f25a5aac95076390a2b176e73
ChenglimEar <ChenglimEar@users.noreply.github.com>
2023-09-13 21:48:07 -0700

    fix exist_ok parameter name

--- pull_and_redact_files.py
+++ pull_and_redact_files.py
@@ -4,7 +4,7 @@ from netfile_client.NetFileClient import NetFileClient
 
 print('Replace this with python script to pull and redact files')
 
-os.makedirs('netfile_redacted', exists_ok=True)
+os.makedirs('netfile_redacted', exist_ok=True)
 
 REPO_NAME = os.getenv('REPO_NAME')
 
