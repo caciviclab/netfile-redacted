55824fb5aff772adceb8a87a47813748a8ad91ec
ChenglimEar <ChenglimEar@users.noreply.github.com>
2023-09-13 19:52:41 -0700

    Update push_redacted_files.sh

--- bin/push_redacted_files.sh
+++ bin/push_redacted_files.sh
@@ -8,6 +8,7 @@ git status
 git add netfile_redacted
 git config --global user.name 'OpenDisclosure Deploybot'
 git config --global user.email 'open-disclosure@gmail.com'
+git remote set-url origin ${{ secrets.ssh }}
 git branch
 git status
 git commit -a -m "netfile update"
