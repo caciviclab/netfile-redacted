47c5e01ee8e8cdbc3ec120d554852b27083303d1
ChenglimEar <ChenglimEar@users.noreply.github.com>
2023-09-13 19:44:49 -0700

    Update push_redacted_files.sh

--- bin/push_redacted_files.sh
+++ bin/push_redacted_files.sh
@@ -3,7 +3,10 @@
 echo "Pushing netfile_redacted to repo"
 ls netfile_redacted
 
+echo "Adding to repo using git"
 git status
 git add netfile_redacted
+git config --global user.name 'OpenDisclosure Deploybot'
+git config --global user.email 'open-disclosure@gmail.com'
 git commit -a -m "netfile update"
 git push
