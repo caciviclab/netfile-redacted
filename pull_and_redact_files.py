9f76759c27435d7481cf1123651de64b8a3c8bf3
Chenglim Ear <chenglim_ear@yahoo.com>
2023-09-13 22:40:05 -0700

    add other data names in comment

--- pull_and_redact_files.py
+++ pull_and_redact_files.py
@@ -2,19 +2,29 @@ import os
 import json
 from netfile_client.NetFileClient import NetFileClient
 
+def redact(data):
+    # TODO: add code that will redact data according to configuration
+    pass
+
 os.makedirs('netfile_redacted', exist_ok=True)
 
 REPO_OWNER = os.getenv('REPO_OWNER')
 
 if REPO_OWNER == 'ChenglimEar':
     print(f'Simulating NetFile response for {REPO_OWNER}')
-    with open('netfile_samples/filer.json','r') as f:
-        data = json.load(f)
-    # TODO: call redaction function
-    with open('netfile_redacted/filer.json','w') as f:
-        json.dump(data,f)
+    for name in ['filers','filings']:
+        with open(f'netfile_samples/{name}.json','r') as f:
+            data = json.load(f)
+            redact(data)
+        with open(f'netfile_redacted/{name}.json','w') as f:
+            json.dump(data,f)
 else:
     print(f'Making NetFile API calls')
     nf = NetFileClient()
-    nf.fetch('filings')
+    for name in ['filers', 'filings']:
+    #for name in ['filers', 'filings','transactions', 'filing_activities', 'filing_elements', 'elections']:
+        data = nf.fetch(name)
+        redact(data)
+        with open(f'netfile_redacted/{name}.json','w') as f:
+            json.dump(data,f)
 
