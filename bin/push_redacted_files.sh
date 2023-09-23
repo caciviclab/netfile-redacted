81e56bc9dd3993bede85b1c609c345861378687d
Chenglim Ear <chenglim_ear@yahoo.com>
2023-09-23 10:52:38 -0700

    remove logic for pushing to repo and always push to Google Drive

--- bin/push_redacted_files.sh
+++
@@ -1,8 +0,0 @@
-# Add bash script to push redacted files into this repository, which will likely live under a netfile_redacted directory
-
-echo "Pushing netfile_redacted to repo"
-git add netfile_redacted
-git config --global user.name 'OpenDisclosure Deploybot'
-git config --global user.email 'open-disclosure@gmail.com'
-git commit -m "netfile update"
-git push
--- test_pull_from_gdrive.py
+++ test_pull_from_gdrive.py
@@ -1,16 +1,14 @@
 import os
 from gdrive_client.GDriveCopier import GDriveCopier
 
-REPO_OWNER = os.getenv('REPO_OWNER','')
 REPO_BRANCH = os.getenv('REPO_BRANCH','_LOCAL_')
 
-if (REPO_OWNER in ['','ChenglimEar']):
-    downloads_dir = '.local/downloads'
-    os.makedirs(downloads_dir, exist_ok=True)
-    copier = GDriveCopier('netfile_redacted', target_branch = REPO_BRANCH)
-    copier.download_to(downloads_dir)
-    print(f'Contents of downloads dir ({downloads_dir}):')
-    local_files = os.listdir(downloads_dir)
-    for local_file in local_files:
-        print(local_file)
-    print('Done')
+downloads_dir = '.local/downloads'
+os.makedirs(downloads_dir, exist_ok=True)
+copier = GDriveCopier('netfile_redacted', target_branch = REPO_BRANCH)
+copier.download_to(downloads_dir)
+print(f'Contents of downloads dir ({downloads_dir}):')
+local_files = os.listdir(downloads_dir)
+for local_file in local_files:
+    print(local_file)
+print('Done')
