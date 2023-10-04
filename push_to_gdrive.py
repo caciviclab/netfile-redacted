093f050382ee84feb97a25d74a2aaa25754ce334
Chenglim Ear <chenglim_ear@yahoo.com>
2023-10-04 09:21:21 -0700

    enable a GDRIVE_FOLDER secret to choose the google drive folder to push and pull data to and from

--- push_to_gdrive.py
+++ push_to_gdrive.py
@@ -2,5 +2,6 @@ import os
 from gdrive_client.GDriveCopier import GDriveCopier
 
 REPO_BRANCH = os.getenv('REPO_BRANCH','_LOCAL_')
-copier = GDriveCopier('netfile_redacted', target_branch=REPO_BRANCH)
+GDRIVE_FOLDER = os.getenv('GDRIVE_FOLDER','netfile_redacted')
+copier = GDriveCopier(GDRIVE_FOLDER, target_branch=REPO_BRANCH)
 copier.upload_from('netfile_redacted')
--- test_pull_from_gdrive.py
+++ test_pull_from_gdrive.py
@@ -2,10 +2,11 @@ import os
 from gdrive_client.GDriveCopier import GDriveCopier
 
 REPO_BRANCH = os.getenv('REPO_BRANCH','_LOCAL_')
+GDRIVE_FOLDER = os.getenv('GDRIVE_FOLDER','netfile_redacted')
 
 downloads_dir = '.local/downloads'
 os.makedirs(downloads_dir, exist_ok=True)
-copier = GDriveCopier('netfile_redacted', target_branch = REPO_BRANCH)
+copier = GDriveCopier(GDRIVE_FOLDER, target_branch = REPO_BRANCH)
 copier.download_to(downloads_dir)
 print(f'Contents of downloads dir ({downloads_dir}):')
 local_files = os.listdir(downloads_dir)
