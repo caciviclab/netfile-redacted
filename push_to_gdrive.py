cebd91147150316914716914d5209dd8202a2e67
Chenglim Ear <chenglim_ear@yahoo.com>
2023-10-05 10:19:07 -0700

    fix upload to gdrive to look in the right place for local files to upload

--- push_to_gdrive.py
+++ push_to_gdrive.py
@@ -4,4 +4,4 @@ from gdrive_client.GDriveCopier import GDriveCopier
 REPO_BRANCH = os.getenv('REPO_BRANCH','_LOCAL_')
 GDRIVE_FOLDER = os.getenv('GDRIVE_FOLDER','netfile_redacted') or 'netfile_redacted'
 copier = GDriveCopier(GDRIVE_FOLDER, target_branch=REPO_BRANCH)
-copier.upload_from('netfile_redacted')
+copier.upload_from('.local/netfile_redacted')
