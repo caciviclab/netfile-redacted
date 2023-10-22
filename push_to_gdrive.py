301846cb95c7702b2bab96daf7d7d219e76f84a0
ChenglimEar <ChenglimEar@users.noreply.github.com>
2023-10-21 22:33:48 -0700

    Use gdrive-datastore (#4)
    The pypi package gdrive-datastore is now a central place for the code
    that allows storing our data files on Google Drive. In this pull
    request, we switch over to a pip install for this code and remove it
    from the repository. Additionally, dev container support has been added
    to ensure the required development environment is completely specified.

--- push_to_gdrive.py
+++ push_to_gdrive.py
@@ -1,7 +1,7 @@
 import os
-from gdrive_client.GDriveCopier import GDriveCopier
+from gdrive_datastore.gdrive import GDriveCopier
 
 REPO_BRANCH = os.getenv('REPO_BRANCH','_LOCAL_')
-GDRIVE_FOLDER = os.getenv('GDRIVE_FOLDER','netfile_redacted') or 'netfile_redacted'
-copier = GDriveCopier(GDRIVE_FOLDER, target_branch=REPO_BRANCH)
+GDRIVE_FOLDER = os.getenv('GDRIVE_FOLDER','OpenDisclosureDev') or 'OpenDisclosureDev'
+copier = GDriveCopier(GDRIVE_FOLDER, target_subfolder=REPO_BRANCH)
 copier.upload_from('.local/netfile_redacted')
