b589c55e4979f4d485e5d6750765aab382ca550c
ckingbailey <ckingbailey@gmail.com>
2023-11-07 22:24:08 -0800

    Fix: get all parts of transaction records (#7)
    This is a repeat of #6. In creating a PR from a fork, I accidentally
    targeted the wrong branch in that PR

--- netfile_client/NetFileClient.py
+++ netfile_client/NetFileClient.py
@@ -37,108 +37,6 @@ class Routes:
     filing_elements = '/filing/v101/filing-elements'
     elections = '/election/v101/elections'
 
-class BaseEndpointClient:
-    """ Base functionality for fetching from NetFile endpoint """
-    def __init__(self, base_url:str, base_params:dict, auth:tuple, additional_params:dict={}):
-        self.has_next_page = True
-        self.base_url = base_url
-        self.url = base_url
-        initial_params = {
-            'offset': 0,
-            'limit': 1000
-        }
-        self.base_params = {
-            **initial_params,
-            **base_params
-        }
-        self.params = {
-            **self.base_params,
-            **additional_params
-        }
-        self.auth = auth
-        self.session = requests.Session()
-        self.session.hooks['response'] = [
-            lambda response, *_, **__: response.raise_for_status()
-        ]
-        retry_strategy = requests.adapters.Retry(total=5, backoff_factor=2)
-        adapter = TimeoutAdapter(max_retries=retry_strategy)
-        self.session.mount('https://', adapter)
-
-    def fetch(self):
-        res = self.session.get(self.url, auth=self.auth, params=self.params)
-        body = res.json()
-        if self.params['offset'] == 0:
-            print(body['totalCount'], flush=True)
-
-        self._configure_next_request(body)
-
-        return body['results']
-    
-    def _configure_next_request(self, response_body):
-        self.has_next_page = response_body['hasNextPage']
-        next_offset = self.params['limit'] + self.params['offset'] if self.has_next_page else None
-
-        if next_offset is not None and callable(self._set_next_fetch_by):
-            try:
-                self._set_next_fetch_by()
-            except StopIteration:
-                print('done', flush=True)
-
-        else:
-            self.params['offset'] = next_offset
-            print(self.params['offset'], self.params['limit'], flush=True)
-
-class FilingElementClient(BaseEndpointClient):
-    """ Fetch filing elements """
-    def __init__(self, base_url, base_params, auth, **kwargs):
-        super().__init__(base_url, base_params, auth)
-        self.url = f'{self.base_url}{Routes.filing_elements}'
-        self._fetchable_by = [
-            'filing_nid',
-            'element_nid'
-        ]
-
-        # Prepare own query attributes
-        self._set_next_fetch_by = self._configure_fetch_by(**kwargs)
-
-    def _configure_fetch_by(self, **kwargs):
-        if len(kwargs) > 1:
-            raise ValueError(f'filing_elements may only be queried by 1 param type, got {kwargs}')
-
-        if len(kwargs) < 1:
-            return None
-
-        fetch_by = list(kwargs.keys())[0]
-        if fetch_by not in self._fetchable_by:
-            raise ValueError(f'Unknown request parameter {fetch_by}')
-
-        return getattr(self, f'_set_next_{fetch_by}')(kwargs[fetch_by])
-
-    def _set_next_filing_nid(self, filing_nids:list[str]=None):
-        """ Add 'filing_nid' to self.params """
-        filing_nids_iter = iter(filing_nids)
-        filing_nid = next(filing_nids_iter)
-        fetch_by = 'filingNid'
-        def set_next():
-            self.params[fetch_by] = filing_nid
-
-        return set_next
-
-    def _set_next_element_nid(self, element_nid):
-        """ Add /{element_nid} to self.url """
-        pass
-
-class FilingClient(BaseEndpointClient):
-    pass
-
-class FilerClient(BaseEndpointClient):
-    pass
-
-class TransactionsClient(BaseEndpointClient):
-    pass
-
-class FilingActivityClient(BaseEndpointClient):
-    pass
 
 class NetFileClient:
     """ Fetch data from NetFile V2 endpoints """
@@ -170,6 +68,7 @@ class NetFileClient:
         self._logger.setLevel(self._log_level)
 
     def get_auth(self, env_file):
+        ''' Get key and secret from env vars or .env file '''
         key_api_key = 'NETFILE_API_KEY'
         key_api_secret = 'NETFILE_API_SECRET'
 
@@ -194,10 +93,12 @@ class NetFileClient:
 
     def fetch(self, endpoint, **kwargs):
         """ Fetch all of a particular record type """
+        self._logger.debug('fetch got kwargs %s', kwargs)
         url = self._base_url + getattr(Routes, endpoint)
         params = { **self._params }
         if 'params' in kwargs:
             params.update(kwargs['params'])
+        self._logger.debug('Fetch %s with params %s', url, params)
         res = self.session.get(url, auth=self._auth, params=params)
         body = res.json()
         results = body['results']
--- pull_and_redact_files.py
+++ pull_and_redact_files.py
@@ -1,7 +1,13 @@
 import os
 import json
+import logging
 import yaml
 from netfile_client.NetFileClient import NetFileClient
+
+LOG_LEVEL = os.getenv('LOG_LEVEL', 'INFO')
+logger = logging.getLogger(__name__)
+logger.addHandler(logging.StreamHandler())
+logger.setLevel(LOG_LEVEL)
 
 class DataRetriever:
     ''' Used to retrieve and redact files
@@ -16,49 +22,59 @@ class DataRetriever:
           NETFILE_API_SECRET: the api secret for accessing NetFile API
         
         '''
-    
+
     def __init__(self, config, dest_dirpath='.local/netfile_redacted'):
         ''' Initialize with redaction configuration, destination directory and NetFile client '''
 
-        self.config = config
+        self.redaction_fields = config['redaction_fields']
+        self.endpoint_spec = config['endpoints']
         self.dest_dirpath = dest_dirpath
 
         NETFILE_API_KEY = os.getenv('NETFILE_API_KEY','')
         NETFILE_API_SECRET = os.getenv('NETFILE_API_SECRET','')
 
         if ((NETFILE_API_KEY != '') and (NETFILE_API_SECRET != '')) or os.path.exists('.env'):
-            print(f'Making NetFile API calls')
-            self.nf = NetFileClient(api_key='',api_secret='')
+            logger.info('Making NetFile API calls')
+            self.nf = NetFileClient()
         else:
-            print(f'Simulating NetFile response since no credentials provided')
+            logger.info('Simulating NetFile response since no credentials provided')
             self.nf = None
 
         os.makedirs(self.dest_dirpath, exist_ok=True)
 
+
     def fetch_and_redact_all(self):
         ''' Get names of content to fetch from NetFile, redact and save '''
 
-        data_keys = self.config['redaction_fields'].keys()
-        for name in data_keys:
-            data = self.fetch(name)
-            self.redact(data, name)
-            with open(f'{self.dest_dirpath}/{name}.json','w') as f:
+        for name, extra_params in self.endpoint_spec.items():
+            logger.info('Get %s', name)
+            data = self.fetch(name, extra_params)
+            logger.info('Got %s of %s rows', name, len(data))
+
+            if name in self.redaction_fields:
+                logger.info('Redact %s', name)
+                self.redact(data, name)
+
+            with open(outpath := f'{self.dest_dirpath}/{name}.json','w', encoding='utf8') as f:
+                logger.info('Save %s to %s', name, outpath)
                 json.dump(data,f,sort_keys=True,indent=1)
 
 
-    def fetch(self,name):
+    def fetch(self,name, extra_params):
         ''' Fetch a specific named content, which may be simulated if NetFile client not initialized '''
 
         if self.nf is None:
             filepath = f'netfile_samples/{name}.json'
             if os.path.exists(filepath):
-                with open(filepath,'r') as f:
+                with open(filepath,'r', encoding='utf8') as f:
                     data = json.load(f)
             else:
                 data = []
         else:
-            data = self.nf.fetch(name)
+            logger.debug('Pass extra_params %s to self.nf.fetch', extra_params)
+            data = self.nf.fetch(name, params=extra_params)
         return data
+
 
     def redact_path(self, data, path):
         ''' Redact a specific entry in the JSON data located through a provided path 
@@ -70,15 +86,16 @@ class DataRetriever:
 
         parts = path.split('.',1)
         if len(parts) == 1:
-            if (type(data) == dict) and (path in data):
+            if isinstance(data, dict) and (path in data):
                 data[path] = '***'
         elif parts[0] == '[]':
-            if (type(data) == list):
+            if isinstance(data, list):
                 for i,v in enumerate(data):
                     self.redact_path(v, parts[1])
         else:
-            if (type(data) == dict) and (parts[0] in data):
+            if isinstance(data, dict) and (parts[0] in data):
                 self.redact_path(data[parts[0]], parts[1])
+
 
     def redact(self, data, data_key):
         ''' Apply all configured redactions for a provided content
@@ -88,16 +105,21 @@ class DataRetriever:
               data_key: the name of the content
             '''
 
-        fields_to_redact = self.config['redaction_fields'][data_key]
+        fields_to_redact = self.redaction_fields[data_key]
 
         for item in data:
             for field_path in fields_to_redact:
                 self.redact_path(item, field_path)
 
-if __name__ == '__main__':
-    with open('config.yaml', 'r') as f:
+
+def main():
+    ''' Pull and redact data based what specified in on config.yaml '''
+    with open('config.yaml', 'r', encoding='utf8') as f:
         config = yaml.safe_load(f)
 
     retriever = DataRetriever(config)
     retriever.fetch_and_redact_all()
 
+
+if __name__ == '__main__':
+    main()
